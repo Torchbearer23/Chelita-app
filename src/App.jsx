4e69a4398e2fc8ea225230eba1a9abc4f919a3db
Alain Lanfranko Alcántara López <alain.lanfranko2808@gmail.com>
2026-02-19 18:36:14 -0500

    Primer commit de la app chelita

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-red-500 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold underline">
+        ¡CHELITA APP VIVA!
+      </h1>
+    </div>
+  )
+}
+
+export default App
