7efe107ff866d0e2edca110f6bd90edcfa407db7
Alain Lanfranko Alcántara López <alain.lanfranko2808@gmail.com>
2026-03-03 21:40:58 -0500

    configuracion de seguridad de la base de datos de chelita

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// URL CORREGIDA (Copiada de tu imagen de configuración)
-const supabaseUrl = 'https://gjvfemikcpezlgkwwfji.supabase.co' 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Asegúrate de copiar la clave ANON de nuevo por si acaso
-const supabaseAnonKey = 'sb_publishable_Z9sT3me7B_dF-GdtLIunuA_MTHfQf6y' 
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
